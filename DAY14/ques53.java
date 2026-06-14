59ad4a4a8e9050b5bb29ad3d9945c5003a4bd2ad
shreeyadhasmana <25114cn358@glbitm.ac.in>
2026-06-14 10:48:57 +0530

    Add files via upload

---
+++ DAY14/ques53.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Q53 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int key = sc.nextInt();
+        int pos = -1;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == key) {
+                pos = i;
+                break;
+            }
+        }
+
+        if (pos != -1)
+            System.out.println("Found at index " + pos);
+        else
+            System.out.println("Not Found");
+    }
+}
---
+++ DAY14/ques55.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Q55 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int largest = arr[0];
+        int secondLargest = arr[0];
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > largest) {
+                secondLargest = largest;
+                largest = arr[i];
+            } else if (arr[i] > secondLargest && arr[i] != largest) {
+                secondLargest = arr[i];
+            }
+        }
+
+        System.out.println("Second Largest = " + secondLargest);
+    }
+}
